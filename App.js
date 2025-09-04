7236aa23a8d8145b621515076327bcf2e44177a1
Gabriel Müller Ferreira Neves <gabrielmnf2311@gmail.com>
2025-09-04 11:51:07 -0300

    [feat] Add Tarefas

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Image, TextInput} from 'react-native';
-import logo from './assets/pngegg (2).png';
+import { StyleSheet, Text, View , Image, TextInput, Alert, TouchableOpacity} from 'react-native';
+import logo from "./assets/to-do-list.png";
 import { useState } from 'react';
 import btnAdd from "./assets/add.png"
 
 export default function App() {
   const [tarefa,setTarefa] = useState("");
+  const [tarefas,setTarefas] = useState ([]);
+
+  const handleAdd = () => {
+    //Alert.alert(tarefa);
+    setTarefas([tarefas, ...tarefas])
+    setTarefa("");
+  }
   return (
     <View style={styles.container}>
       <View>
         <Image source={logo} style={styles.logo}/> 
       <Text>TODO list</Text>
         </View>
-        <View>
+        <View style={styles.viewInput}>
           <TextInput placeholder="Entre com a tarefa"
           value={tarefa}
           onChangeText={setTarefa}/>
-          <Image source={btnAdd}/>
+          <TouchableOpacity onPress={handleAdd}>
+          <Image source={btnAdd} style={styles.btnAdd}/>
+          </TouchableOpacity>
         </View>
       <StatusBar style="auto" />
     </View>
   );
+  //ja importei o flash
 }
 
 const styles = StyleSheet.create({
@@ -29,13 +39,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
   logo: {
-    height: 120,
-    weidth: 128,
+    height: 128,
+    width: 128,
   },
   btnAdd: {
     width: 32,
     height: 32,
+  },
+  viewInput: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width:"100%"
   }
 });
